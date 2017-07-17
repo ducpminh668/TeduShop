0833275d542c39f0e843a9c0bb3d8e8008e99587
DESKTOP-5MP9QR3\MinhDuc <ducpminh668@gmail.com>
2017-07-17 20:49:32 +0700

    Add Api and use Autofac

--- TeduShop.Data/Repositories/ErrorRepository.cs
+++ TeduShop.Data/Repositories/ErrorRepository.cs
@@ -3,14 +3,14 @@ using TeduShop.Model.Models;
 
 namespace TeduShop.Data.Repositories
 {
-    //public interface IErrorRepository : IRepository<Error>
-    //{
-    //}
+    public interface IErrorRepository : IRepository<Error>
+    {
+    }
 
-    //public class ErrorRepository : RepositoryBase<Error>, IErrorRepository
-    //{
-    //    public ErrorRepository(IDbFactory dbFactory) : base(dbFactory)
-    //    {
-    //    }
-    //}
+    public class ErrorRepository : RepositoryBase<Error>, IErrorRepository
+    {
+        public ErrorRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+    }
 }
